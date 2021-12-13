eb6a492c9c452e44221cdeb733f49de2cf3d1da1
Ragados <ragados@gmail.com>
2021-12-13 09:27:32 -0800

    Initial commit - Project creation.

---
+++ main.py
@@ -0,0 +1,7 @@
+# This is a project for me to experiment with an array of Python Coding Exercises
+# The intention of this project is to help me explore an increasing range of features
+# as well as to continue to test and grow my coding ability and discipline
+# Dev Sodagar
+# Creation Date: 13 December 2021
+# Last Updated: 13 December 2021
+
